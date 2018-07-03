91b4f1bee05127f4fe54347a98d1e80c28cfdbc0
guilherme  henrique boing <guilhermeboing@hotmail.com>
2018-07-03 11:37:43 -0600

    0.0 version

---
+++ redeprog/ip.php
@@ -0,0 +1,18 @@
+<?php
+
+ $sttrinca = $_GET['1trinca'];
+ $ndtrinca = $_GET['2trinca'];
+ $rdtrinca = $_GET['3trinca'];
+ $thtrinca = $_GET['4trinca'];
+ $masc    = $_GET['masc'];
+ 
+ echo $sttrinca;
+ echo "<hr>";
+ echo $ndtrinca;
+ echo "<hr>";
+ echo $rdtrinca;
+ echo "<hr>";
+ echo $thtrinca;
+ echo "<hr>";
+ echo $masc;
+ echo "<hr>";
---
+++ redeprog/script.js
@@ -0,0 +1,31 @@
+// test button
+$(document).ready(function(){
+     $("#test").click(function(){
+       alert("tudo em ordem com o jquery!")
+     });
+});
+
+$(document).ready(function(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
